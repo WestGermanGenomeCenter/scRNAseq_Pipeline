a43423d14e46b0a42a39c769415b4580041e8877
My Ky Huynh <my.ky.huynh@uni-duesseldorf.de>
2022-01-13 03:33:17 +0100

    Skip cluster in DGE if 1 condition has < 3 cells

--- scripts/DGE.R
+++ scripts/DGE.R
@@ -18,10 +18,17 @@ for(i in 0:len) {
   cluster <- SetIdent(cluster, value=condition)
   #Find Markers between both conditions
   if(length(levels(cluster)) == length(metaIdents)) {
-    cluster.markers <- FindMarkers(cluster, only.pos=F, min.pct=0.25, logfc.threshold=0.25, ident.1=metaIdents[[1]], ident.2=metaIdents[[2]])
-    #plot_in_terminal(plotname=paste("../projects/", projectDirName, "plots/", i, ".cluster.dge.pdf", sep=""),
-    #               to_plot=DoHeatmap(cluster, features=cluster.markers))
-    write.csv(cluster.markers, file=paste(projectDirPath, "csv/", project, ".compareGE_inCluster", i, ".markers.csv", sep=""))
+    cluster.conditions <- SplitObject(cluster, split.by=condition)
+    if(length(colnames(cluster.conditions[[1]])) < 3) {
+      txtContent <- paste(txtContent, paste("Cluster", i, "was skipped since", levels(cluster.conditions[[1]])[[1]], "has less than 3 cells", sep=" "), sep="\n")
+    } else if(length(colnames(cluster.conditions[[2]])) < 3) {
+      txtContent <- paste(txtContent, paste("Cluster", i, "was skipped since", levels(cluster.conditions[[2]])[[1]], "has less than 3 cells", sep=" "), sep="\n")
+    } else {
+      cluster.markers <- FindMarkers(cluster, only.pos=F, min.pct=0.25, logfc.threshold=0.25, ident.1=metaIdents[[1]], ident.2=metaIdents[[2]])
+      #plot_in_terminal(plotname=paste("../projects/", projectDirName, "plots/", i, ".cluster.dge.pdf", sep=""),
+      #               to_plot=DoHeatmap(cluster, features=cluster.markers))
+      write.csv(cluster.markers, file=paste(projectDirPath, "csv/", project, ".compareGE_inCluster", i, ".markers.csv", sep=""))
+    }
   } else {
     txtContent <- paste(txtContent, paste("Cluster", i, "was skipped since it was made up of only", levels(cluster)[[1]], sep=" "), sep="\n")
   }
