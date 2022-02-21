1a871c8f27ae79b9c00427e5b08dfb25ea17c086
My Ky Huynh <my.ky.huynh@uni-duesseldorf.de>
2022-02-21 01:14:17 +0100

    Add env-cleaner.py

---
+++ envs/env-cleaner.py
@@ -0,0 +1,15 @@
+
+files = ["counting-spec.yml", "devtools-spec.yml", "doublet-spec.yml", "env-spec.yml", "marker-spec.yml", "shiny-spec.yml"]
+
+for filename in files:
+    textlines = []
+    with open(filename) as f:
+        textlines = f.readlines()
+    index = 0
+    while("dependencies:" not in textlines[index]):
+        index += 1
+    for i in range(index, len(textlines)):
+        j = rfind("=")
+        textlines = textlines[0:j]
+    with open(filename, "w") as f:
+        f.write("\n".join(textlines))
